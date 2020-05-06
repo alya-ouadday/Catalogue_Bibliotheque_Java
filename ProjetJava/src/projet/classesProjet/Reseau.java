fff1a7d3e31c358e876dce4951f3a2f39ca9374d
alya-chan <63308468+alya-chan@users.noreply.github.com>
2020-05-06 17:40:03 +0200

    reglage hashmap author avec ArrayList<Document>

--- ProjetJava/src/projet/classesProjet/Reseau.java
+++ ProjetJava/src/projet/classesProjet/Reseau.java
@@ -13,22 +13,27 @@ public class Reseau {
 	private ArrayList<Bibliotheque> listeBiblio;
 	private HashMap<String, Document> listeDocument;
 	private HashMap<String, Livre> listeLivre;
-	private HashMap<String, Document> listeAuthor;
+	private HashMap<String, ArrayList<Document>> listeAuthor;
 	private HashMap<String, Serie> listeSerie;
 	
 	public Reseau(ArrayList<Bibliotheque> listeBiblio, HashMap<String, Document> listeDocument, 
-				   HashMap<String, Livre> listeLivre, HashMap<String, Document> listeAuthor, HashMap<String, Serie> listeSerie) {
+				   HashMap<String, Livre> listeLivre, HashMap<String, ArrayList<Document>> listeAuthor, HashMap<String, Serie> listeSerie) {
 		this.listeBiblio = listeBiblio;
 		this.listeDocument = listeDocument;
 		this.listeLivre = listeLivre;
 		this.listeAuthor = listeAuthor;
 		this.listeSerie = listeSerie;	
 	}
+	
 	public Reseau() {
-		this(new ArrayList<Bibliotheque>(), new HashMap<String, Document>(), new HashMap<String, Livre>(),
-				new HashMap<String, Document>(), new HashMap<String, Serie>());
-				
+		
+		this(new ArrayList<Bibliotheque>(), new HashMap<String,Document>(), new HashMap<String, Livre>(),
+				new HashMap<String,ArrayList<Document>>(), new HashMap<String, Serie>()) ;
+		
+
 	}
+				
+	
 	
 	
 	public void addBiblio(Bibliotheque bibliotheque) {
