dbee59997648933b613240311e850f76004a4e0e
dxhunterx <64537122+dxhunterx@users.noreply.github.com>
2020-05-26 17:03:48 +0200

    searchserie

--- ProjetJava/src/projet/classesProjet/Serie.java
+++ ProjetJava/src/projet/classesProjet/Serie.java
@@ -6,26 +6,32 @@ import java.util.TreeMap;
 public class Serie {
 	private String name;
 	private HashMap<Document, Integer> listeDoc;
+	private HashMap<Document, Integer> listeDocDate;
 	
-	public Serie(String name, HashMap<Document, Integer> listeDoc) {
+	public Serie(String name, HashMap<Document, Integer> listeDoc, HashMap<Document, Integer> listeDocDate) {
 		this.name = name;
 		this.listeDoc = listeDoc;
+		this.listeDocDate = listeDocDate;
 	}
 	public Serie(String name) {
-		this(name, new HashMap<Document, Integer>());
+		this(name, new HashMap<Document, Integer>(),new HashMap<Document, Integer>());
 	}
-	
-	public HashMap<Document, Integer> getListeDoc(){
-		return listeDoc; 
-	}
-	
-	public void addDocument(Document document, Integer numero) {
-		if(document instanceof InSerie) {
-			listeDoc.put(document, numero); 
-		}
-	}
-	
 	public String getName() {
 		return name; 
 	}
+	public HashMap<Document, Integer> getListeDoc(){
+		return listeDoc; 
+	}
+	public HashMap<Document, Integer> getListeDocDate(){
+		return listeDocDate; 
+	}
+	
+	public void addDocument(Document document, Integer numero,Integer date) {
+		if(document instanceof InSerie) {
+			listeDoc.put(document, numero); 
+			listeDocDate.put(document, date);
+		}
+	}
+	
+	
 }
