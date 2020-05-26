ef382d2ad22031fd8ae72d581b27171fafba4bed
dxhunterx <64537122+dxhunterx@users.noreply.github.com>
2020-05-26 16:04:52 +0200

    debut menu

--- ProjetJava/src/projet/application/Main.java
+++ ProjetJava/src/projet/application/Main.java
@@ -2,6 +2,7 @@ package projet.application;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Scanner;
 
 import projet.data.FileReader;
 import projet.exceptions.formatEANException;
@@ -10,6 +11,46 @@ import projet.classesProjet.*;
 
 public class Main 
 {
+	public static void afficheMenu() {
+		System.out.println("Choisissez le numéro de votre commande : ");
+		System.out.println("1 - ajouter une nouvelle bibliothèque au reseau ");
+		System.out.println("2 - ajouter document");
+		System.out.println("3 - ajouter nouvel utilisateur");
+		System.out.println("4 - afficher l'ensemble des document ");
+		System.out.println("5 - afficher les document d'une série");
+		System.out.println("6 - afficher les documents d'un auteur");
+		System.out.println("7 - afficher un livre par son ISBN");
+		System.out.println("8 - afficher un document par son EAN");
+		System.out.println("9 - afficher le nombre de doc entre deux date");
+		
+	}
+	public static void menu(Reseau reseau) {
+		Scanner sc = new Scanner(System.in);
+		
+		boolean marche = true;
+		int commande = 0;
+		while(marche) {
+			//si on est dans le menu
+			if(commande ==0) {
+				afficheMenu();
+				System.out.println("Veuillez choisir une commande :");
+				String str = sc.nextLine();
+				System.out.println("Vous avez choisis, : " + str);
+				commande = Integer.parseInt(str);
+			}
+			//si commande 1
+			if(commande ==1) {
+				System.out.println("Veuillez choisir une commande :");
+				String str = sc.nextLine();
+				System.out.println("Vous avez choisis, : " + str);
+				commande = Integer.parseInt(str);
+			}
+			
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) 
 	{
 		
@@ -48,12 +89,8 @@ public class Main
 		{
 			System.out.println("[Main] You should enter the CSV file path as a parameter.");
 		}
-	
 		
-		
-		
-		
-		
+		/*
 		//parisBiblios.searchDocumentsAuthorSurname("Boris"); 
 		parisBiblios.searchNumberPeriod(2008, 2010);
 		
@@ -64,7 +101,8 @@ public class Main
 		jeanPierreMelville.searchNumberPeriod(2008, 2010);
 		oscardWilde.searchNumberPeriod(2008, 2010);
 		saintSimon.searchNumberPeriod(2008, 2010);
-
+		*/
+		menu(parisBiblios);
 		
 		
 		
