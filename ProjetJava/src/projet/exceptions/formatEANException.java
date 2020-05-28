3046f6e125176f5c188d71dc11bb59953434ff4e
dxhunterx <64537122+dxhunterx@users.noreply.github.com>
2020-05-28 23:40:15 +0200

    exception

--- ProjetJava/src/projet/exceptions/formatEANException.java
+++ ProjetJava/src/projet/exceptions/formatEANException.java
@@ -1,7 +1,15 @@
 package projet.exceptions;
 
+/**
+ * Exception si le format d'un EAN n'est as respecté
+ * @author BEN OUADDAY et LEJEUNE
+ *
+ */
 public class formatEANException extends Exception {
 
+	/**
+	 * Constructeur
+	 */
 	public formatEANException() {
 		super("L'EAN donné n'a pas le bon format de 13 chiffres"); 
 	}
--- ProjetJava/src/projet/exceptions/quotaException.java
+++ ProjetJava/src/projet/exceptions/quotaException.java
@@ -1,7 +1,15 @@
 package projet.exceptions;
 
+/**
+ * Exception si le quota d'un utiisateur est dépassé
+ * @author BEN OUADDAY et LEJEUNE
+ *
+ */
 public class quotaException extends Exception{
 
+	/**
+	 * Constructeur
+	 */
 	public quotaException() {
 		super("Votre quota est épuisé, vous ne pouvez plus emprunter"); 
 	}
